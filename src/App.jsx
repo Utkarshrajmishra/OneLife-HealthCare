1de339784a6a8756b4b7ab47a311dee28880c470
Utkarsh Raj Mishra <utkarshrajmishra811545@gmail.com>
2024-08-01 00:18:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-primary">Hello world</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      colors:{
+        primary:"#113D3C",
+        tuseary:"#3AB449",
+        secondary:"#E7F34A",
+        red:"#EC3B3B",
+        textColor:"#25262F",
+        paragraph:"#4D4D4D",
+        stock:"#F4F4F4",
+      }
+    },
+  },
+  plugins: [],
+}
+
